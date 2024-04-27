f56adf273557e11701997d37759fa00ac837c0be
Alan O'Regan <72257010+alan-oregan@users.noreply.github.com>
2024-04-27 16:11:20 +0100

    add generateTasksAPI

--- function_app.py
+++ function_app.py
@@ -1,28 +1,80 @@
+import json
 import azure.functions as func
 import logging
 
+from langchain_community.llms import HuggingFaceEndpoint
+from langchain_core.prompts import PromptTemplate
+
+from typing import List
+from langchain_core.pydantic_v1 import BaseModel, Field
+from langchain_core.output_parsers import JsonOutputParser
+
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
+hub = HuggingFaceEndpoint(repo_id="NousResearch/Nous-Hermes-2-Mixtral-8x7B-DPO")
 
-@app.route(route="trigger")
-def trigger(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info("Python HTTP trigger function processed a request.")
+tasks_template = """<|im_start|>system
+You are a helpful designer for a childrens digital game company<|im_end|>
+<|im_start|>user
+{name} is {age} years old and has a reading level age of {reading_level}. They need help with {teaching_task}.
 
-    name = req.params.get("name")
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get("name")
+Make a list of 10 suitable game scene tasks to learn {teaching_task}.
 
-    if name:
+Just list out each item 1 by 1 as a JSON list. Only provide the list of tasks, do not include the question or any other information, just the list of tasks.<|im_end|>
+<|im_start|>assistant
+"""
+
+tasks_prompt = PromptTemplate(
+    template=tasks_template,
+    input_variables=["name", "age", "reading_level", "teaching_task"],
+)
+
+
+class Tasks(BaseModel):
+    tasks: List = Field(description="list of game scene tasks")
+
+
+parser = JsonOutputParser(pydantic_object=Tasks)
+
+tasks_prompt_json = PromptTemplate(
+    template=tasks_template,
+    input_variables=["name", "age", "reading_level", "teaching_task"],
+    partial_variables={"format_instructions": parser.get_format_instructions()},
+)
+
+tasks_chain = tasks_prompt_json | hub | parser
+
+
+@app.route(route="generateTasks")
+def generateTasks(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info("generateTasks function processed a request.")
+
+    try:
+        req_body = req.get_json()
+        name = req_body.get("name")
+        age = req_body.get("age")
+        reading_level = req_body.get("reading_level")
+        teaching_task = req_body.get("teaching_task")
+    except ValueError:
+        pass
+
+    if name and age and reading_level and teaching_task:
         return func.HttpResponse(
-            f"Hello, {name}. This HTTP triggered function executed successfully."
-        )
-    else:
-        return func.HttpResponse(
-            "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+            json.dumps(
+                tasks_chain.invoke(
+                    {
+                        "name": name,
+                        "age": age,
+                        "reading_level": reading_level,
+                        "teaching_task": teaching_task,
+                    }
+                )
+            ),
             status_code=200,
+            mimetype="application/json",
         )
+
+    return func.HttpResponse(
+        "Pass {name, age, reading_level, teaching_task} in the request body for generated tasks.",
+        status_code=200,
+    )
